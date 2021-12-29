777ddeff97821c1d537901f63ef57aeb8575b71f
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-12-29 15:21:09 +0700

    make route for campaign

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['middleware' => 'auth','prefix' => 'api'], function ($router) 
+{
+    $router->get('me', 'AuthController@me');
+
+    // Campaign
+    $router->get('/campaign', 'CampaignController@index');
+    $router->post('/campaign/add', 'CampaignController@makeCampaign');
+});
+
+$router->group(['prefix' => 'api'], function () use ($router) 
+{
+   $router->post('register', 'AuthController@register');
+   $router->post('login', 'AuthController@login');
+});
