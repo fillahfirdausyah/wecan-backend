d22ca3d298e17d15786c9ed0279d03193a501b27
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-12-29 11:27:20 +0700

    make mogration

---
+++ database/migrations/2021_12_29_030248_create_campaign_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampaignTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaign', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('desccription');
+            $table->date('campaign_start');
+            $table->bigInteger('campaign_goal');
+            $table->bigInteger('campaign_collected');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaign');
+    }
+}
---
+++ database/migrations/2021_12_29_040509_create_donation_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDonationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donation', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('campaign_id');
+            $table->bigInteger('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donation');
+    }
+}
