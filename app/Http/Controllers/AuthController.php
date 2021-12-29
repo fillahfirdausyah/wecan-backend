6ee47b4e42525f4641137fd47eaec95ee2f9d67a
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-12-29 14:16:42 +0700

    make JWT AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+        // $this->middleware('jwt_verify', ['except' => ['login','register']]);
+        
+    }
+
+    /**
+     * Store a new user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function register(Request $request)
+    {
+        //validate incoming request 
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        try 
+        {
+            $user = new User;
+            $user->username= $request->input('username');
+            $user->password = app('hash')->make($request->input('password'));
+            $user->save();
+
+            return response()->json( [
+                        'entity' => 'users', 
+                        'action' => 'create', 
+                        'result' => 'success',
+            ], 201);
+
+        } 
+        catch (\Exception $e) 
+        {
+            return response()->json( [
+                       'entity' => 'users', 
+                       'action' => 'create', 
+                       'result' => 'failed',
+                       'error' => $e
+            ], 409);
+        }
+    }
+	
+     /**
+     * Get a JWT via given credentials.
+     *
+     * @param  Request  $request
+     * @return Response
+     */	 
+    public function login(Request $request)
+    {
+          //validate incoming request 
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only(['username', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {			
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+	
+     /**
+     * Get user details.
+     *
+     * @param  Request  $request
+     * @return Response
+     */	 	
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function respondWithToken($token)
+    {
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ], 200);
+    }
+}
