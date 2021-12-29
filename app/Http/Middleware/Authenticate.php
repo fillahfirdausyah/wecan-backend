53ce550776f1f14d0540638a375353d0a37aa71f
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-12-29 15:20:25 +0700

    change respons message

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -36,7 +36,7 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
+            return response()->json(['status' => 'Unauthorized', 'code' => '401'], 401);
         }
 
         return $next($request);
