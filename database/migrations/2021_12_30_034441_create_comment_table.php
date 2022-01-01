7b1ce99bccdc293e113ae651905687bee3e06482
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-01 20:37:42 +0700

    make comment migration

---
+++ database/migrations/2021_12_30_034441_create_comment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('campaign_id');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment');
+    }
+}
