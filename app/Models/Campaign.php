ebe8beeb7cb7a6b0bfdad04052effd555f4ac376
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-04 14:02:34 +0700

    make relationship to comment

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -39,4 +39,8 @@ class Campaign extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function comment() {
+        return $this->hasMany(Comment::class);
+    }
 }
