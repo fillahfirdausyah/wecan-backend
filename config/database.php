b6ec1033c7f17713497e14065586e592f341ab8b
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-07 12:52:41 +0700

    pqsql driver

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+   'default' => 'accounts',
+
+   'connections' => [
+        'pqsql' => [
+            'driver'    => 'pqsql',
+            'host'      => env('DB_HOST'), 
+            'port'      => env('DB_PORT'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+         ],
+    ],
+];
